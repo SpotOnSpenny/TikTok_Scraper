3955ba3b1b4cadd3974072087219731e9f5c422e
Appologetic <spencer.fietz@gmail.com>
2024-11-06 13:35:45 -0700

    found bug: tiktok length not being found until long after it first appears

--- src/tiktok_scraper/Core/processing.py
+++ src/tiktok_scraper/Core/processing.py
@@ -2,6 +2,7 @@
 import time
 
 # External Dependency Imports
+from selenium.webdriver.common.by import By
 
 # Internal Dependency Imports
 from tiktok_scraper.Core.global_vars import monitoring_data
@@ -13,6 +14,15 @@ from tiktok_scraper.Core.selenium_utils import next_video
 
 def process_ad(driver):
     # Check the length of the ad, and start timer
+    tiktok_element = driver.find_element(By.ID, f"one-column-item-{monitoring_data['data_index']}")
+    print(monitoring_data['data_index'])
+    tiktok_length = "00:00 / 00:00"
+    while tiktok_length == "00:00 / 00:00":
+        tiktok_element = driver.find_element(By.ID, f"one-column-item-{monitoring_data['data_index']}")
+        tiktok_length = tiktok_element.find_element(By.XPATH, "//p[contains(@class, 'StyledTUXText')]")
+        print(tiktok_length.get_attribute("outerHTML"))
+        tiktok_length = tiktok_length.get_attribute("innerText")
+    print("..." + tiktok_length + "...")
     # Save screenshots of the ad (10x per second) until timer is over
     # Piece together screenshots into a video   
     # Save the video to disk
